be57384776b9c0c6f561f4913a5d2bd41bf4ee7d
Dan Gilleland <dgilleland@nait.ca>
2017-12-01 08:49:23 -0700

    Classes with demo implementation
    TODO: supply some sort of Sequence diagram support, such as
    [mermaid](https://github.com/knsv/mermaid)

---
+++ DocFx Markdown Plugins/DocMarkdown.AsSequence/SequenceDiagramBlockCodeRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.DocAsCode.Dfm;
+using Microsoft.DocAsCode.MarkdownLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocMarkdown.AsSequence
+{
+    public class SequenceDiagramBlockCodeRenderer : DfmCustomizedRendererPartBase<IMarkdownRenderer, MarkdownCodeBlockToken, MarkdownBlockContext>
+    {
+        public override string Name => "SequenceDiagramRendererPart";
+
+        public override bool Match(IMarkdownRenderer renderer, MarkdownCodeBlockToken token, MarkdownBlockContext context)
+        {
+            return token.Lang == "sequence";
+        }
+
+        public override StringBuffer Render(IMarkdownRenderer renderer, MarkdownCodeBlockToken token, MarkdownBlockContext context)
+        {
+            StringBuffer result = "<pre><code class=\"";
+            result += renderer.Options.LangPrefix;
+            result += "csharp";
+            result += "\">";
+            result += token.Code;
+            result += "\n</code></pre>";
+            return result;
+        }
+    }
+}
