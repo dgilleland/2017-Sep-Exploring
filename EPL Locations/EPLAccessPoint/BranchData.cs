b7ed9c096a2d04a38c7dc5bc80cf6900733b20ee
Dan Gilleland <dgilleland@nait.ca>
2018-03-13 11:58:43 -0600

    EPL Web Data experiment

---
+++ EPL Locations/EPLAccessPoint/BranchData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPLAccessPoint
+{
+    // https://www.epl.ca/open-data-branches-api-help/
+    public class BranchData
+    {
+        private const string Url = @"https://www2.epl.ca/OpenData/Branches/Files/";
+        private enum QueryStringParam { branch, format }
+        private static readonly IDictionary<QueryStringParam, string> DefaultValues
+            = new Dictionary<QueryStringParam, string>();
+        static BranchData()
+        {
+            DefaultValues.Add(QueryStringParam.branch, "ALL");
+            DefaultValues.Add(QueryStringParam.format, "xml");
+        }
+
+        public static string QueryUrl(string branch, string format)
+        {
+            branch = branch ?? DefaultValues[QueryStringParam.branch];
+            format = format ?? DefaultValues[QueryStringParam.format];
+            return $"{Url}?{QueryStringParam.branch}={branch}&{QueryStringParam.format}={format}";
+        }
+    }
+
+    public class Hours
+    {
+        public string DayOfWeek { get; set; }
+        public string Open { get; set; }
+        public string Close { get; set; }
+    }
+    public class HolidayClosure
+    {
+        public string HolidayName { get; set; }
+        public DateTime HolidayDate { get; set; }
+    }
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+    public class EPLBranches
+    {
+        public IEnumerable<BranchInfo> BranchInfo { get; set; }
+    }
+    public class BranchInfo
+    {
+        public string BranchId { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string ImageUrl { get; set; }
+        public IEnumerable<Hours> BranchHours { get; set; }
+        public IEnumerable<HolidayClosure> HolidayClosures { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public Location Coordinates { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+
+}
---
+++ EPL Locations/WebApp/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using EPLAccessPoint;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                PopulateLibraryData();
+            }
+        }
+
+        private void PopulateLibraryData()
+        {
+            // From https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-request-data-using-the-webrequest-class
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create(BranchData.QueryUrl(null, null));
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.  
+            reader.Close();
+            response.Close();
+
+
+            // From https://stackoverflow.com/a/39902334
+            var doc = new XmlDocument();
+            doc.LoadXml(responseFromServer); //or XDocument.Load(path)
+            string jsonText = JsonConvert.SerializeXmlNode(doc.LastChild, Newtonsoft.Json.Formatting.Indented, true);
+            var dyn = JsonConvert.DeserializeObject<EPLBranches>(jsonText);
+
+            // Bind to display
+            //EPLBranchView.DataSource = dyn.BranchesInfo
+            //EPLBranchView.DataBind();
+        }
+    }
+}
